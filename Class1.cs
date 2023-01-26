a26cb63dd017456736be3b8377d8152ba9191981
Ilkin Masimli <ilkin.masimli98@gmail.com>
2023-01-25 23:14:31 -0800

    first commit

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Building
+    {
+        public string name;
+        public float height;
+        public float area;
+        public string address;
+
+        public Building(string name, float height, float area, string address)
+        {
+            this.name = name;
+            this.height = height;
+            this.area = area;
+            this.address = address;
+        }
+
+        public string GetDetails()
+        {
+            return $"{name} {height} {area} {address}";
+        }
+
+        public float GetVolume()
+        {
+            return (area * height);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var building1 = new Building("Empire State", 300, 10, "somewhere in New York");
+            Console.WriteLine(building1.GetVolume());
+
+            var building2 = new Building("Big ben", 150, 6, "somewhere in London");
+            Console.WriteLine(building2.GetVolume());
+
+
+        }
+    }
+}
